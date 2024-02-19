daf68b8669706abe5d580ead44e921251f276bdd
Ezio-33 <8691@holbertonstudents.com>
2024-02-19 14:06:07 +0000

    mise a jour

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - imprime la phrase "Programmer, c'est comme construire un puzzle multilingue".
+ * void est un type de données spécial qui est utilisé pour indiquer l'absence de type.
+ * Retour : 0 si la sortie s'est faite correctement, non nul sinon
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
