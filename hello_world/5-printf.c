a55ffdd936d4e0f0055b16a35a6624a6c9a4f4bd
Ezio-33 <8691@holbertonstudents.com>
2024-02-19 14:56:45 +0000

    mise a jour

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,1 +1,11 @@
-5-printf.c
+#include <stdio.h>
+/**
+ * main - imprime la phrase avec une grammaire correcte,
+ * mais le résultat est une œuvre d'art,
+ * Return: 0 si la sortie s'est faite correctement.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
