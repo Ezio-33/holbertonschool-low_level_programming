e125d9f7e5b37de0280dc183077f02e7dcaf3765
Ezio-33 <8691@holbertonstudents.com>
2024-02-20 13:06:50 +0000

    mise a jour

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,22 @@
-3-print_alphabets.c
+#include <stdio.h>
+
+/**
+ * main - imprime l'alphabet en minuscules.
+ * Return: Toujours 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+		for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
