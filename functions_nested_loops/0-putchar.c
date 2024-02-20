99f0ed334e86e3fcfbaa9d29b099ea47ed7f4e88
Ezio-33 <8691@holbertonstudents.com>
2024-02-20 18:27:59 +0000

    mise a jour

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Affiche _putchar, suivi d'une nouvelle ligne
+ * Return: Toujours 0
+ */
+int main(void)
+{
+	char *str = "_putchar\n";
+
+	while (*str)
+		_putchar(*str++);
+	return (0);
+}
