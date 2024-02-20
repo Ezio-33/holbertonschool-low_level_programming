b0f4feb7f20198c1df6dbbdd375f69faee859d4e
Ezio-33 <8691@holbertonstudents.com>
2024-02-20 15:53:18 +0000

    mise a jour

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,18 @@
-7-print_tebahpla.c
+#include <stdio.h>
+
+/**
+ * main - imprime l'alphabet minuscule à l'envers, suivi d'une nouvelle ligne.
+ * Return: Toujours 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
