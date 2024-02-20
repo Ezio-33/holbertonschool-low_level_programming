8d072e3e79d2a44c974f3a1b54bc34d1db052a40
Ezio-33 <8691@holbertonstudents.com>
2024-02-20 15:32:35 +0000

    mise a jour

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,17 @@
-6-print_numberz.c
+#include <stdio.h>
+
+/**
+ * main - imprime tous les chiffres de 0 à 9, suivi d'une nouvelle ligne.
+ * Return: Toujours 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		putchar('\n');
+	}
+	return (0);
+}
