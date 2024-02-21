4d6fa3b4b21978abbc8e3e09406e8e9b2f2300bd
Ezio-33 <8691@holbertonstudents.com>
2024-02-21 13:46:13 +0000

    mise a jour

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,10 +6,15 @@
  */
 int main(void)
 {
-	char *str = "_putchar\n";
-
-	while (*str)
-		putchar(*str++);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	putchar('\n');
+
 	return (0);
 }
