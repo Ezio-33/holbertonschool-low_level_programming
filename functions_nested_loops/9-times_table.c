577b12b8c7ac7c8104423ca15f31f6f46f7ab798
Ezio-33 <8691@holbertonstudents.com>
2024-02-21 21:08:59 +0000

    mise a jour

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,1 +1,29 @@
-9-times_table.c
+#include "main.h"
+
+/**
+ * times_table - affiche la table de multiplication par 9, en commençant par 0
+ */
+void times_table(void)
+{
+	int i, resultat;
+
+	for (i = 0; i <= 9; i++)
+	{
+	resultat = i * 9;
+		if (resultat < 10)
+		{
+			_putchar(resultat + '0');
+		}
+		else
+		{
+			_putchar((resultat / 10) + '0');
+			_putchar((resultat % 10) + '0');
+		}
+		if (i != 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
