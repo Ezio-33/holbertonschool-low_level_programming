6b93afd1d42413d9f1efd007c60a42a2f21bffe5
Ezio-33 <8691@holbertonstudents.com>
2024-02-21 22:01:12 +0000

    mise a jour

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,1 +1,16 @@
-10-add.c
+#include "main.h"
+
+/**
+ * add - additionne deux entiers et renvoie le résultat
+ * @entiers_1: int à ajouter à entiers_2
+ * @entiers_2: à ajouter à entiers_1
+ * Return: sommes de entiers_1 et entiers_2
+ */
+int add(int entiers_1, int entiers_2)
+{
+	int resultat;
+
+	resultat = entiers_1 + entiers_2;
+
+	return (resultat);
+}
