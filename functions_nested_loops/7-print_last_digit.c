866394f4f78a520ae918ad29590ea93a89605dba
Ezio-33 <8691@holbertonstudents.com>
2024-02-21 20:11:52 +0000

    mise a jour

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,18 @@
-7-print_last_digit.c
+#include "main.h"
+/**
+ * print_last_digit - Imprime le dernier chiffre d'un nombre et le retourne
+ * @nombre: Le nombre dont on veut imprimer et retourner le dernier chiffre
+ * Return: La valeur du dernier chiffre du nombre
+ */
+int print_last_digit(int nombre)
+{
+	int dernier_chiffre;
+
+	dernier_chiffre = nombre % 10;
+		if (dernier_chiffre < 0)
+		{
+		dernier_chiffre = -dernier_chiffre;
+		}
+		_putchar(dernier_chiffre + '0');
+	return (dernier_chiffre);
+}
