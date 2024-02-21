ad86bfe76fd51bdb9a00b47b8b40d300e39b72db
Ezio-33 <8691@holbertonstudents.com>
2024-02-21 15:05:56 +0000

    mise a jour

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
-
 /**
- * print_alphabet - Affiche l'alphabet en minuscules
+ * print_alphabet - Affiche l'alphabet en minuscules
  * suivi d'une nouvelle ligne
  * Return: Toujours 0
- */
+ */
+
 void print_alphabet(void)
 {
 	char lettre = 'a';
