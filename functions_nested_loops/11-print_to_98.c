b45367fbaabac550a69fd779e0f7e62df812ea57
Ezio-33 <8691@holbertonstudents.com>
2024-02-21 22:18:27 +0000

    mise a jour

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,34 @@
-11-print_to_98.c
+#include "main.h"
+/**
+ * print_to_98 - affiche tous les nombres naturels de
+ * nombres à 98, suivis d'un saut de ligne
+ * @nombres: le nombre à partir duquel commencer l'impression
+ */
+void print_to_98(int nombres)
+{
+	if (nombres <= 98)
+	{
+		while (nombres < 98)
+		{
+			_putchar((nombres / 10) + '0');
+			_putchar((nombres % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+			nombres++;
+		}
+	}
+	else
+	{
+		while (nombres > 98)
+		{
+			_putchar((nombres / 10) + '0');
+			_putchar((nombres % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+			nombres--;
+		}
+	}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
+}
