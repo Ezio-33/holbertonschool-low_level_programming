61855303b4c59a852c0cb884db474d3062b24c50
Ezio-33 <8691@holbertonstudents.com>
2024-02-21 17:11:31 +0000

    mise a jour

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,1 +1,18 @@
-3-islower.c
+#include "main.h"
+
+/**
+ * _islower - Vérifier si un caractère est minuscule.
+ * @Lettre: Le caractère à vérifier.
+ * Return: 1 si c est en minuscules, 0 si en maguscule.
+ */
+int _islower(int Lettre)
+{
+	if (Lettre >= 'a' && Lettre <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
