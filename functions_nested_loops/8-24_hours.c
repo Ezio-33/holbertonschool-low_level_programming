c01560c1c9953ea8732ab15d428c9b9fc8f05411
Ezio-33 <8691@holbertonstudents.com>
2024-02-21 20:44:38 +0000

    mise a jour

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,1 +1,25 @@
-8-24_hours.c
+#include "main.h"
+
+/**
+ * jack_bauer - affiche chaque minute de la journée de Jack Bauer
+ */
+void jack_bauer(void)
+{
+	int heure = 0, minute = 0;
+
+		while (heure < 24)
+		{
+		minute = 0;
+		while (minute < 60)
+			{
+			_putchar((heure / 10) + '0');
+			_putchar((heure % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+			minute++;
+			}
+		heure++;
+		}
+}
