a9b692108694b8dcb54adc52d5a7604024bddf9d
Ezio-33 <8691@holbertonstudents.com>
2024-02-21 15:37:37 +0000

    mise a jour

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,22 @@
-2-print_alphabet_x10.c
+#include "main.h"
+/**
+ * print_alphabet_x10 - Affiche l'alphabet en minuscules 10 fois
+ * suivi d'une nouvelle ligne à chaque itération
+ */
+void print_alphabet_x10(void)
+{
+	char lettre;
+	int i = 0;
+
+	while (i < 10)
+	{
+		lettre = 'a';
+		while (lettre <= 'z')
+		{
+			_putchar(lettre);
+			lettre++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
