965d09e090f53693141cb0fd8d73b4e10408ddb5
Ezio-33 <8691@holbertonstudents.com>
2024-02-23 09:54:56 +0000

    mise a jour

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,1 +1,14 @@
-101-quote.c
+#include <unistd.h>
+/**
+* int main(void) : Ceci est la fonction principale du programme. Elle renvoie un entier et ne prend aucun argument.
+* char message[] est utilisé pour créer un tableau avec le message que l'on souhaite afficher.
+* 2: Représente la sortie d'erreur standard
+* sizeof(message) - 1: nombre d'octets à écrire, en excluant le caractère de fin de chaînequi est nul noté \0
+*/
+
+
+int main(void) {
+ char message[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+ write(2, message, sizeof(message) - 1);
+ return 1;
+}
