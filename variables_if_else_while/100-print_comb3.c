170bdf9b1a0ea6f785d5f90fad00a1bf07ca0390
Ezio-33 <8691@holbertonstudents.com>
2024-02-23 13:53:55 +0000

    mise a jour

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,30 @@
-100-print_comb3.c
+#include <stdio.h>
+/**
+* main - Affiche toutes les combinaisons
+* différentes possibles de deux chiffres
+* Return: Toujours 0 (Succès)
+*/
+
+int main(void)
+{
+	int nombre1, nombre2;
+
+	for (nombre1 = 0; nombre1 < 10; nombre1++)
+	{
+		for (nombre2 = nombre1 + 1; nombre2 < 10; nombre2++)
+		{
+			putchar(nombre1 + '0');
+			putchar(nombre2 + '0');
+
+			if (nombre1 < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,32 @@
-101-print_comb4.c
+#include <stdio.h>
+/**
+* main - affiche toutes les combinaisons différentes
+*possibles de trois chiffres
+* Return: Toujours 0 (Succès)
+*/
+int main(void)
+{
+	int nombre1, nombre2, nombre3;
+
+	for (nombre1 = 0; nombre1 < 10; nombre1++)
+	{
+		for (nombre2 = nombre1 + 1; nombre2 < 10; nombre2++)
+		{
+			for (nombre3 = nombre2 + 1; nombre3 < 10; nombre3++)
+			{
+				putchar(nombre1 + '0');
+				putchar(nombre2 + '0');
+				putchar(nombre3 + '0');
+
+				if (!(nombre1 == 7 && nombre2 == 8 && nombre3 == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
