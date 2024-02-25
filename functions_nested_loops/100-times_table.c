0befcf02f67a2678044163d2dcc4b67dc1b8b9c6
Ezio-33 <8691@holbertonstudents.com>
2024-02-25 21:50:43 +0000

    mise a jour

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,35 +1,48 @@
-#include <stdio.h>
 #include "main.h"
 /**
 * print_times_table - imprime la table de n fois, en commençant par 0
-* @nombre1: nombre de la table de multiplication
+* @n: nombre de la table de multiplication
 */
+#include "main.h"
 
-void print_times_table(int n) {
-	if (n < 0 || n > 15)
-		return;
+void print_times_table(int n)
+{
+	int i, j, k;
 
-	for (int i = 0; i <= n; i++) {
-		for (int j = 0; j <= n; j++) {
-			int product = i * j;
-			if (j != 0) {
-				_putchar(',');
-				_putchar(' ');
-				if (product < 10) {
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-				} else if (product < 100) {
 					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
-			if (product >= 100) {
-				_putchar(product / 100 + '0');
-				_putchar((product / 10) % 10 + '0');
-			} else if (product >= 10) {
-				_putchar(product / 10 + '0');
-			}
-			_putchar(product % 10 + '0');
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,1 +1,30 @@
-101-natural.c
+#include <stdio.h>
+
+/**
+ * main - calcule et affiche la somme de tous les multiples
+ * de 3 ou 5 inférieurs à 1024
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int sum3, sum5, sum;
+	int i;
+
+	sum = 0;
+	sum3 = 0;
+	sum5 = 0;
+
+	for (i = 0; i < 1024; ++i)
+	{
+		if ((i % 3) == 0)
+		{
+			sum3 = sum3 + i;
+		} else if ((i % 5) == 0)
+		{
+			sum5 = sum5 + i;
+		}
+	}
+	sum = sum3 + sum5;
+	printf("%lu\n", sum);
+	return (0);
+}
