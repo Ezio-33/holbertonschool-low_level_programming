d7db7985f1d841a3646c1ed3e9b7bc8d77c6c7b1
Ezio-33 <8691@holbertonstudents.com>
2024-02-27 18:39:13 +0000

    Tasks 2 et 3

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,19 @@
-2-strlen.c
+#include "main.h"
+/**
+* _strlen - Calcule la longueur d'une chaîne de caractères.
+* @s: Le pointeur vers la chaîne de caractères
+* dont on veut connaître la longueur.
+* Return: La longueur de la chaîne de caractères (entier positif).
+*/
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,1 +1,15 @@
-3-puts.c
+#include "main.h"
+/**
+*_puts - Affiche une chaîne de caractères suivie d'un saut de ligne.
+* @str: Le pointeur vers la chaîne de caractères à afficher.
+*/
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
+
