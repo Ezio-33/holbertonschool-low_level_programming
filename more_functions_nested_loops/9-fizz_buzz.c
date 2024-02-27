ccff2dab953bfa33d8f5cae4a7a1930163499e77
Ezio-33 <8691@holbertonstudents.com>
2024-02-27 11:03:14 +0000

    mise a jour

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,39 @@
-9-fizz_buzz.c
+#include "main.h"
+
+/**
+ * main - Point d'entrée du programme
+ * Return: Toujours 0
+ */
+int main(void)
+{
+	int nombre = 1;
+
+	while (nombre <= 100)
+	{
+		if (nombre % 3 == 0 && nombre % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (nombre % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (nombre % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", nombre);
+		}
+		if (nombre != 100)
+		{
+			printf(" ");
+		}
+
+		nombre++;
+	}
+	printf("\n");
+
+	return (0);
+}
