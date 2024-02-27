a50055e98071da59b59c053cc60ac1fd2b2bdd8f
Ezio-33 <8691@holbertonstudents.com>
2024-02-27 10:43:04 +0000

    mise a jour

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,30 @@
-8-print_square.c
+#include "main.h"
+
+/**
+* print_square - affiche un carré de taille n
+* @size: taille du carré
+*/
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int nb1 = 0;
+
+		while (nb1 < size)
+		{
+			int nb2 = 0;
+
+			while (nb2 < size)
+			{
+				_putchar('#');
+				nb2++;
+			}
+			_putchar('\n');
+			nb1++;
+		}
+	}
+}
