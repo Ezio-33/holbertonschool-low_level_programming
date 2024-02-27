07e336a2b83216f119efddbee6cd7e69797b1c4f
Ezio-33 <8691@holbertonstudents.com>
2024-02-27 19:42:38 +0000

    tasks 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,35 @@
-5-rev_string.c
+#include "main.h"
+
+/**
+ * rev_string - Inverse une chaîne de caractères.
+ * @chaine: La chaîne de caractères à inverser.
+ */
+void rev_string(char *chaine)
+{
+	int longueur = 0;
+	int debut, fin;
+	char temp;
+
+/**Calculer la longueur de la chaîne de caractères */
+	while (chaine[longueur] != '\0')
+	{
+		longueur++;
+	}
+
+/** Initialiser les variable de début et de fin pour inverser la chaîne*/
+	debut = 0;
+	fin = longueur - 1;
+
+/** Inverser la chaîne de caractères en échangeant les caractères*/
+	while (debut < fin)
+	{
+/** Échanger les caractères aux variable début et fin*/
+		temp = chaine[debut];
+		chaine[debut] = chaine[fin];
+		chaine[fin] = temp;
+
+/** Déplacer les variable vers le milieu de la chaîne*/
+		debut++;
+		fin--;
+	}
+}
