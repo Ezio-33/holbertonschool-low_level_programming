48a0241bbfaa5eebb80f368e029a9246d83ab2cb
Ezio-33 <8691@holbertonstudents.com>
2024-02-27 17:41:20 +0000

    task 0 et 1

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,11 @@
-0-reset_to_98.c
+#include "main.h"
+
+/**
+ * reset_to_98 - met à jour la valeur de la variable
+ * le pointeur pointe vers 98
+ * @n: pointeur sur la variable à mettre à jour
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,1 +1,16 @@
-1-swap.c
+#include "main.h"
+
+/**
+* swap_int - échange les valeurs de deux entiers
+* @entiers1: pointeur sur la première valeur
+* @entiers2: pointeur sur la deuxième valeur
+* temp: variable temporaire pour stocker le nombre a switch
+*/
+void swap_int(int *entiers1, int *entiers2)
+{
+	int temp;
+
+	temp = *entiers1;
+	*entiers1 = *entiers2;
+	*entiers2 = temp;
+}
