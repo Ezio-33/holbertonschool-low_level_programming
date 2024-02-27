6559200473021df2d93d05760d199f0ff25b44b5
Ezio-33 <8691@holbertonstudents.com>
2024-02-27 09:44:29 +0000

    mise a jour

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,24 @@
-5-more_numbers.c
+#include "main.h"
+/**
+* more_numbers - affiche 10 fois les nombres
+* de 0 à 14, suivis d'un saut de ligne.
+*/
+
+void more_numbers(void)
+{
+	int nombre1 = 0, nombre2;
+
+	while (nombre1 < 10)
+	{
+		nombre2 = 0;
+		while (nombre2 <= 14)
+		{
+			if (nombre2 > 9)
+				_putchar(nombre2 / 10 + '0');
+			_putchar(nombre2 % 10 + '0');
+			nombre2++;
+		}
+		_putchar('\n');
+		nombre1++;
+	}
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -9,5 +9,10 @@ int _isdigit(int nombre);
 int mul(int nombre1, int nombre2);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 
 #endif
