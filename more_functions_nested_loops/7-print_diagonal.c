f0b6da15ed085b25b304d3a7aa4cd4d38486c360
Ezio-33 <8691@holbertonstudents.com>
2024-02-27 10:25:40 +0000

    mise a jour

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,27 @@
-7-print_diagonal.c
+#include "main.h"
+
+/**
+ * print_diagonal - dessine une ligne diagonale de longueur n
+ * @nb_barres_obliques: nombre de fois où '\' doit être imprimé
+ */
+void print_diagonal(int nb_barres_obliques)
+{
+	if (nb_barres_obliques <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int ligne, espaces;
+
+		for (ligne = 0; ligne < nb_barres_obliques; ligne++)
+		{
+			for (espaces = 0; espaces < ligne; espaces++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
