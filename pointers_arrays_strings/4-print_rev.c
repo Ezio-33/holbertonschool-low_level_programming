d2028cc820c13e8bedfaae525d71ae36b001cba2
Ezio-33 <8691@holbertonstudents.com>
2024-02-27 19:08:10 +0000

    task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,21 @@
-4-print_rev.c
+#include "main.h"
+
+/**
+* print_rev - Imprime une chaîne de caractères à
+*l'envers, suivie d'un saut de ligne.
+* @chaine: Chaîne de caractères à imprimer à l'envers.
+*/
+void print_rev(char *chaine)
+{
+	int longueur = 0;
+
+	while (*(chaine + longueur) != '\0')
+	{
+		longueur++;
+	}
+	for (int position = longueur - 1; position >= 0; position--)
+	{
+		_putchar(*(chaine + position));
+	}
+	_putchar('\n');
+}
