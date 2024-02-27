639a6043c9b76b933a7b18fd43522ebc5bb58e0d
Ezio-33 <8691@holbertonstudents.com>
2024-02-27 09:54:58 +0000

    mise a jour

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,23 @@
-6-print_line.c
+#include "main.h"
+
+/**
+ * print_line - affiche une ligne droite de longueur n
+ * @n: nombre de fois où le caractère "_" doit être affiché
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
