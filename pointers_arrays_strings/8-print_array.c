18736651ccc4f48e5ed5716f4b50b6d0422f44a5
Ezio-33 <8691@holbertonstudents.com>
2024-02-28 16:38:02 +0000

    task 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,21 @@
-8-print_array.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_array - Affiche les éléments d'un tableau
+*d'entiers, séparés par une virgule et un espace.
+* @tableau: pointeur vers le tableau d'entiers
+* @n: nombre d'éléments à afficher
+*/
+void print_array(int *tableau, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != 0)
+			printf(", ");
+		printf("%d", tableau[i]);
+	}
+	printf("\n");
+}
