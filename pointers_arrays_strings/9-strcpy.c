a74c01579095147011c9e007191a5a110eed019c
Ezio-33 <8691@holbertonstudents.com>
2024-02-28 17:19:03 +0000

    tasks 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,22 @@
-9-strcpy.c
+#include "main.h"
+/**
+* _strcpy - Copie la chaîne source dans le tampon de destination.
+* @dest: Pointeur vers le tampon de destination.
+* @src: Pointeur vers la chaîne source à copier.
+* Return: Le pointeur vers le tampon de destination.
+*/
+char *_strcpy(char *dest, char *src)
+{
+	char *debut = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+
+	*dest = '\0';
+
+	return (debut);
+}
