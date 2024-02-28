fbf482f49e428f4d1825fcf1c4eebf66a1e1cc09
Ezio-33 <8691@holbertonstudents.com>
2024-02-28 09:31:48 +0000

    tasks 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,19 @@
-6-puts2.c
+#include "main.h"
+
+/**
+* puts2 - Imprime chaque deuxième caractère d'une chaîne,
+* en commençant par le premier caractère, suivi d'un saut de ligne.
+* @chaine: La chaîne de caractères dont les caractères alternés
+* doivent être imprimés.
+*/
+void puts2(char *chaine)
+{
+	int i = 0;
+
+	while (chaine[i] != '\0')
+	{
+		_putchar(chaine[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
