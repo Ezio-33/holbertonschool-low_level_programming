1e8020f6cdbaee3d5b3a33761edd5ed15969bd23
Ezio-33 <8691@holbertonstudents.com>
2024-02-28 12:53:01 +0000

    correction tasks 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,31 +1,30 @@
 #include "main.h"
 
 /**
-* puts_half - Imprime la deuxième moitié de la chaîne de caractères.
-* @chaine: La chaîne de caractères dont la deuxième moitié doit
-* être imprimée.
-*/
+ * puts_half - Imprime la deuxième moitié de la chaîne de caractères.
+ * @chaine: La chaîne de caractères dont la deuxième moitié doit être imprimée.
+ */
 void puts_half(char *chaine)
 {
 	int longueur = 0;
-	int i, debut;
+	int debut;
 
 	while (chaine[longueur] != '\0')
 	{
 		longueur++;
 	}
-
 	if (longueur % 2 == 0)
 	{
 		debut = longueur / 2;
 	}
 	else
 	{
-		debut = (longueur - 1) / 2;
+		debut = (longueur + 1) / 2;
 	}
-	for (i = debut; i < longueur; i++)
+	while (chaine[debut] != '\0')
 	{
-		_putchar(chaine[i]);
+		_putchar(chaine[debut]);
+		debut++;
 	}
 	_putchar('\n');
 }
