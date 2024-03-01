059f3ff05195512ac80798985e24d37e2a2f15cc
Ezio-33 <8691@holbertonstudents.com>
2024-03-01 10:47:04 +0000

    tasks 0 correction

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,26 @@
 #include "main.h"
 /**
- * _strcat - Concatène deux chaînes
- * @dest: Chaîne de destination
- * @src: Chaîne source à concaténer
- * Return: Pointeur vers la chaîne résultante dest
- */
+* _strcat - Concatène deux chaînes
+* @dest: Chaîne de destination
+* @src: Chaîne source à concaténer
+* Return: Pointeur vers la chaîne résultante dest
+*/
 char *_strcat(char *dest, char *src)
 {
-    int longueur_dest = 0;
-    int longueur_src = 0;
+	int longueur_dest = 0;
+	int longueur_src = 0;
 
-    while (dest[longueur_dest] != '\0')
-    {
-        longueur_dest++;
-    }
+	while (dest[longueur_dest] != '\0')
+	{
+		longueur_dest++;
+	}
 
-    while (src[longueur_src] != '\0')
-    {
-        dest[longueur_dest] = src[longueur_src];
-        longueur_dest++;
-        longueur_src++;
-    }
-    dest[longueur_dest] = '\0';
-    return (dest);
+	while (src[longueur_src] != '\0')
+	{
+		dest[longueur_dest] = src[longueur_src];
+		longueur_dest++;
+		longueur_src++;
+	}
+	dest[longueur_dest] = '\0';
+	return (dest);
 }
