ef9a02bb98cffc3989f22ea77b06ab1696efad7a
Ezio-33 <8691@holbertonstudents.com>
2024-03-01 15:11:52 +0000

    tasks 6 et 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,1 +1,29 @@
-7-leet.c
+#include "main.h"
+
+/**
+* leet - Encode une chaîne en 1337
+* @s: Chaîne à encoder
+* Return: La chaîne encodée
+*/
+char *leet(char *s)
+{
+	int i, j;
+	char lettres[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char remplacements[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j = 0;
+		while (lettres[j] != '\0')
+		{
+			if (s[i] == lettres[j])
+			{
+				s[i] = remplacements[j];
+				break;
+			}
+		j++;
+		}
+	}
+	return (s);
+}
+
