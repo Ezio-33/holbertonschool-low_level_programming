04561b22d651605593c69079beee111d41ef49ff
Ezio-33 <8691@holbertonstudents.com>
2024-03-01 12:15:55 +0000

    tasks 1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,27 @@
-1-strncat.c
+#include "main.h"
+
+/**
+* _strncat - Concatène deux chaînes en utilisant au plus n caractères de src
+* @dest: Chaîne de destination
+* @src: Chaîne source à concaténer
+* @n: Nombre maximal de caractères à utiliser de src
+* Return: Pointeur vers la chaîne résultante dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int longueur_dest = 0;
+	int longueur_src = 0;
+
+	while (dest[longueur_dest] != '\0')
+	{
+		longueur_dest++;
+	}
+	while (src[longueur_src] != '\0' && longueur_src < n)
+	{
+		dest[longueur_dest] = src[longueur_src];
+		longueur_dest++;
+		longueur_src++;
+	}
+	dest[longueur_dest] = '\0';
+	return (dest);
+}
