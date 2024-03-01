c05b3979aaa10500af267d6daca950c63e199649
Ezio-33 <8691@holbertonstudents.com>
2024-03-01 14:32:22 +0000

    tasks 4 et 5

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,20 @@
-4-rev_array.c
+#include "main.h"
+
+/**
+* reverse_array - inverse un tableau d'entiers
+* @a : tableau à inverser
+* @n : nombre d'éléments dans le tableau
+*/
+void reverse_array(int *a, int n)
+{
+	int i, j, tmp;
+
+	j = n - 1;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j--] = tmp;
+	}
+}
--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,20 @@
-5-string_toupper.c
+#include "main.h"
+
+/**
+* string_toupper - transforme toutes les lettres minuscules d'une chaîne
+* en majuscules
+* @s : chaîne à modifier
+* Return: la chaîne de caractères
+*/
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+
+	return (s);
+}
