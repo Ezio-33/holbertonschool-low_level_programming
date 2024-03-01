6911d6c5b1ea98683032c4139c771e5603706ab3
Ezio-33 <8691@holbertonstudents.com>
2024-03-01 14:18:46 +0000

    tasks 2 et 3

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,26 @@
-2-strncpy.c
+#include "main.h"
+
+/**
+* _strncpy - Copie au plus n caractères de src dans dest
+* @dest: Chaîne de destination
+* @src: Chaîne source à copier
+* @n: Nombre maximal de caractères à copier de src
+* Return: Pointeur vers la chaîne résultante dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,22 @@
-3-strcmp.c
+#include "main.h"
+
+/**
+* _strcmp - compare deux chaînes de caractères
+* @s1: première chaîne à comparer
+* @s2 : deuxième chaîne à comparer
+* Return: inférieur à 0 si s1 est inférieur à s2, 0 s'ils sont égaux,
+* supérieur à 0 si s1 est supérieur à s2
+*/
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
