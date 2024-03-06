f29bb9f7d6aea2142b1a3fd7e82ea71a1160fe29
Ezio-33 <8691@holbertonstudents.com>
2024-03-06 15:12:54 +0000

    tasks 0 projet 2

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Point d'entrée du programme
+ * @argc: Le nombre d'arguments en ligne de commande
+ * @argv: Un tableau contenant les arguments
+ * de la ligne de commande
+ *
+ * Return: 0 (Succès)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
