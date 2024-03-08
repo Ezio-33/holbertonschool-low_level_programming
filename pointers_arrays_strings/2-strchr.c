84114ee9f5f283bc40a21db30f97407960e9c365
Ezio-33 <8691@holbertonstudents.com>
2024-03-08 14:30:55 +0000

    mise a jour

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
-* *_strchr - Cherche un caractère dans une chaîne de caractè.
+* *_strchr - Cherche un caractère dans une chaîne de caracter.
 * @chaine: Chaîne de caractères dans laquelle chercr.
 * @caractere: Caractère a recherche.
-* Return: Pointe vers la première occurrence du caract�
-* ou NULL si non trouv�.
+* Return: Pointe vers la première occurrence du caract
+* ou NULL si non trouv.
 */
 char *_strchr(char *chaine, char caractere)
 {
-	while (*chaine != '\0')
+	while (*chaine++)
 	{
 		if (*chaine == caractere)
 		{
 			return (chaine);
 		}
-		chaine++;
-
 	}
 	return ('\0');
 }
