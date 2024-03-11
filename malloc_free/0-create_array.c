4fb8b4327661c44d198e488eab8070f59b60ab35
Samuel <86527262+Ezio-33@users.noreply.github.com>
2024-03-11 12:51:13 +0100

    tasck 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,4 +1,29 @@
-#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
 /**
-  *
+ * create_array - Crée un tableau de caractères et 
+ * l'initialise avec un caractère spécifique
+ * @taille: Taille du tableau à créer
+ * @c: Caractère avec lequel initialiser le tableau
+ *
+ * Return: (Un pointeur vers le tableau créé, ou NULL en cas d'échec)
  */
+char *create_array(unsigned int taille, char c)
+{
+    char *tableau;
+    unsigned int i;
+
+    if (taille == 0)
+        return (NULL);
+
+    tableau = malloc(sizeof(char) * taille);
+    if (tableau == NULL)
+        return (NULL);
+
+    else 
+      for (i = 0; i < taille; i++)
+        tableau[i] = c;
+
+    return (tableau);
+}
