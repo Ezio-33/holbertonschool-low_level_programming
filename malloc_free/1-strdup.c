21e8ee10bcf6a2bda2fe8ce4294fb5ee3a8c6771
Ezio-33 <8691@holbertonstudents.com>
2024-03-11 12:59:03 +0000

    tasks 1 projet 0

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,4 +1,34 @@
-#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
 /**
-  *
+ * _strdup - Duplique une chaîne de caractères
+ * @chaine: Chaîne à dupliquer
+ * Return: (Un pointeur vers la nouvelle chaîne dupliquée, ou NULL en cas d'échec)
  */
+ 
+char *_strdup(char *chaine)
+{
+	char *duplique;
+	unsigned int longueur = 0;
+	unsigned int i;
+
+	if (chaine == NULL)
+		return (NULL);
+
+	while (chaine[longueur] != '\0')
+		longueur++;
+
+	duplique = malloc(sizeof(char) * (longueur + 1));
+
+	if (duplique == NULL)
+		return (NULL);
+
+	for (i = 0; i < longueur; i++)
+		duplique[i] = chaine[i];
+
+	duplique[longueur] = '\0';
+
+	return (duplique);
+}
+
