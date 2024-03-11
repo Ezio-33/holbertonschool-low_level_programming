ee025540f6e66c2fb21c78afa94eebe59b7954a0
Ezio-33 <8691@holbertonstudents.com>
2024-03-11 16:13:28 +0000

    task 4 projet 0

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,4 +1,23 @@
-#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
 /**
-  *
+ * free_grid - Libère la mémoire allouée pour une grille à deux dimensions
+ * @grille: Pointeur vers la grille à libérer
+ * @hauteur: Hauteur de la grille
  */
+void free_grid(int **grille, int hauteur)
+{
+	int i;
+
+	if (grille == NULL)
+		return;
+
+	for (i = 0; i < hauteur; i++)
+	{
+		free(grille[i]);
+	}
+
+	free(grille);
+}
+
