d165593aaa4ef1c8627bdfded42db49aa92523c9
Ezio-33 <8691@holbertonstudents.com>
2024-03-12 09:53:45 +0000

    tasks 0 projet 1

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,20 @@
-#
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - alloue de la mémoire en utilisant malloc
+ * @b: nombre d'octets à allouer
+ *
+ * Return: un pointeur vers la mémoire allouée
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
+
