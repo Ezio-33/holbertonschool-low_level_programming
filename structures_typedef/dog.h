8695de1a8655f50d1acf25630615bd7d9cd739a0
Samuel <86527262+Ezio-33@users.noreply.github.com>
2024-03-12 16:37:42 +0100

    preparation projet 2

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Informations de base du chien
+ * @name: Premier elements
+ * @age: Deuxième elements
+ * @owner: Troisième elements
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
