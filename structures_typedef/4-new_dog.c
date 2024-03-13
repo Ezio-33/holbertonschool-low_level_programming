322637d04240ad38ad3317a91f072999a7200dcf
Ezio-33 <8691@holbertonstudents.com>
2024-03-13 19:37:36 +0000

    correction tasks 4 projet 2

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,36 +1,96 @@
-#include "dog.h"
 #include <stdlib.h>
-#include <string.h>
+#include "dog.h"
 
 /**
- * new_dog - Crée un nouveau chien
- * @name: Nom du chien
- * @age: Âge du chien
- * @owner: Propriétaire du chien
- * Return: Pointeur vers le nouveau chien créé, NULL en cas d'échec
+ * _strlen - retourne la longueur d'une
+ * chaîne de caractères
+ * @s: chaîne à évaluer
+ *
+ * Return: la longueur de la chaîne
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * *_strcpy - copie la chaîne pointée par src
+ * y compris le caractère nul de fin (\0)
+ * dans le tampon pointé par dest
+ * @dest: pointeur vers le tampon dans
+ * lequel nous copions la chaîne
+ * @src: chaîne à copier
+ *
+ * Return: le pointeur vers dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - crée un nouveau chien
+ * @name: nom du chien
+ * @age: âge du chien
+ * @owner: propriétaire du chien
+ *
+ * Return: pointeur vers le nouveau chien
+ * sinon, NULL
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog = malloc(sizeof(dog_t));
+	dog_t *dog;
+	int len1, len2;
 
-	if (new_dog == NULL)
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
 		return (NULL);
 
-	new_dog->name = strdup(name);
-	if (new_dog->name == NULL)
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
 	{
-		free(new_dog);
+		free(dog);
 		return (NULL);
 	}
 
-	new_dog->age = age;
-	new_dog->owner = strdup(owner);
-	if (new_dog->owner == NULL)
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
 	{
-		free(new_dog->name);
-		free(new_dog);
+		free(dog);
+		free(dog->name);
 		return (NULL);
 	}
 
-	return (new_dog);
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
 }
