b1d5c03dbd57e440a165fa9cfe9c0783f25d97f5
Ezio-33 <8691@holbertonstudents.com>
2024-03-13 17:32:17 +0000

    correction tasks 2 projet 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -3,18 +3,18 @@
 
 /**
  * print_dog - Affiche les informations d'un chien
- * @chien: Pointeur vers la structure dog à afficher
+ * @d: Pointeur vers la structure dog à afficher
  */
-void print_dog(struct dog *chien)
+void print_dog(struct dog *d)
 {
-	if (chien != NULL)
+	if (d != NULL)
 	{
-		printf("Nom: %s\n", (chien->name != NULL)
-								? chien->name
+		printf("Nom: %s\n", (d->name != NULL)
+								? d->name
 								: "(nil)");
-		printf("Age: %.1f\n", chien->age);
-		printf("Propriétaire: %s\n", (chien->owner != NULL)
-										 ? chien->owner
+		printf("Age: %.1f\n", d->age);
+		printf("Propriétaire: %s\n", (d->owner != NULL)
+										 ? d->owner
 										 : "(nil)");
 	}
 	else
