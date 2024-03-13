b6032b23e010d3fb73515adecabdc2bab648d08d
Ezio-33 <8691@holbertonstudents.com>
2024-03-13 16:27:15 +0000

    mise a jour dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,7 +6,6 @@
  * @name: Premier elements
  * @age: Deuxième elements
  * @owner: Troisième elements
- * dog_t - typedef pour struct dog
  */
 struct dog
 {
@@ -15,7 +14,12 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - typedef pour struct dog
+ */
+
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
