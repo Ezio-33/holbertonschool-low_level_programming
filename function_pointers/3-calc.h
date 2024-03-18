99d2a5f8659ee75fd4a53e3b8f8c59e7b04bc80b
Ezio-33 <8691@holbertonstudents.com>
2024-03-18 18:01:09 +0000

    task 3 projet zero fichier cal.h

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,6 +1,23 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef CALC_H
+#define CALC_H
 
+/**
+ * struct op - Struct op
+ *
+ * @op: L'opérateur
+ * @f: La fonction associée
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
 
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
 #endif
