c8751cfed73b3b71bee095375f3252b05b4a38db
Ezio-33 <8691@holbertonstudents.com>
2024-03-19 14:22:13 +0000

    task 2 projet 1

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - affiche les chaînes de caractères
+ * suivies d'un saut de ligne.
+ * @separator: Chaîne à afficher entre les chaînes
+ * @n: Nombre de chaînes passées à la fonction
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *str;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(args);
+}
