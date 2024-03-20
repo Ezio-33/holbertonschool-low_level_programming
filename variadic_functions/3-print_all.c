481ee6cf1356f54286c430f3b72306d1d4626f7f
Ezio-33 <8691@holbertonstudents.com>
2024-03-20 13:06:23 +0000

    projet 1 tasks 3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - Affiche des données en fonction du format spécifié.
+ * @format: Liste de types d'arguments passés à la fonction
+ */
+void print_all(const char *const format, ...)
+{
+	va_list args;
+	char *separator = "";
+	unsigned int i = 0;
+	char c;
+	char *str;
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		c = format[i];
+		switch (c)
+		{
+		case 'c':
+			printf("%s%c", separator, va_arg(args, int));
+			break;
+		case 'i':
+			printf("%s%d", separator, va_arg(args, int));
+			break;
+		case 'f':
+			printf("%s%f", separator, va_arg(args, double));
+			break;
+		case 's':
+			separator = ", ";
+			str = va_arg(args, char *);
+			if (str == NULL)
+				printf("%s(nil)", separator);
+			else
+				printf("%s%s", separator, str);
+			break;
+		default:
+			i++;
+			continue;
+		}
+		i++;
+		separator = ", ";
+	}
+
+	printf("\n");
+	va_end(args);
+}
