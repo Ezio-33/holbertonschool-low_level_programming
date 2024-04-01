423ce5ee38fb61940d3c42cc9a46ea9dd2c9b5e7
Ezio-33 <8691@holbertonstudents.com>
2024-04-01 21:25:41 +0000

    Preparation projet 0 et mise a jour du scripte git_auto-V3

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+NAME
+        printf -
+    Formatting and displaying text
+
+        SYNOPSIS
+#include <stdio.h>
+
+    int
+    printf(const char *format, ...);
+
+DESCRIPTION
+
+FORMAT SPECIFIERS
+
+        EXAMPLES OF USAGE :
+
+    EXAMPLE OUTPUT :
+
+    RETURN VALUE
+
+        ERRORS
+
+            AUTHORS
+                VERSCHUEREN Samuel
+
+                    Source code available at : < https: // github.com/Ezio-33/holbertonschool-low_level_programming>
