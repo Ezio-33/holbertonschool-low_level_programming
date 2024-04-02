6471e89f64c7654ae426d99456dd56b2f0137f1a
Ezio-33 <8691@holbertonstudents.com>
2024-04-02 08:21:57 +0000

    fix Singly linked lists tasks 0

--- singly_linked_lists/README.md
+++ singly_linked_lists/README.md
@@ -1,1 +1,22 @@
-C - More malloc, free
+NAME
+
+C - Singly linked lists
+
+SYNOPSIS
+   
+
+DESCRIPTION
+
+FORMAT SPECIFIERS
+
+EXAMPLES OF USAGE :
+
+EXAMPLE OUTPUT :
+
+RETURN VALUE
+ERRORS
+
+AUTHORS
+    VERSCHUEREN Samuel
+
+Source code available at : < https: // github.com/Ezio-33/holbertonschool-low_level_programming>
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS.H
-#define LISTS .H
+#ifndef LISTS_H
+#define LISTS_H
 #include <stddef.h>
 
 /**
@@ -17,6 +17,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
