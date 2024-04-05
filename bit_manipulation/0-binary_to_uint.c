cd5399758d1c46fa3e07c832620843e6ff65b0d9
Ezio-33 <8691@holbertonstudents.com>
2024-04-05 12:37:24 +0000

    C - Bit manipulation tasks 0 correction

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,33 +1,37 @@
 #include "main.h"
 
 /**
- * binary_to_uint - convertit une chaîne
- *  de caractères binaire en un entier non signé
- * @b: chaîne de caractères binaire
- *
- * Return: la valeur convertie ou 0 si
- * la chaîne est vide ou contient des caractères
+ * binary_to_uint - Convertit une chaîne
+ * de caractères binaire en un entier non signé
+ * @b: Chaîne de caractères binaire
+ * Return: L'entier converti ou 0 si la
+ * chaîne est vide ou contient des caractères
  * autres que '0' ou '1'
  */
-unsigned int
-binary_to_uint(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+	unsigned int resultat = 0;
+	int puissance = 1;
 	int i = 0;
 
-	if (*b == '\0')
+	if (!b)
 		return (0);
 
 	while (b[i] != '\0')
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		if (b[i] == '1')
-			result = result * 2 + 1;
-		else
-			result *= 2;
 		i++;
 	}
 
-	return (result);
+	i--;
+	while (i >= 0)
+	{
+		if (b[i] == '1')
+			resultat += puissance;
+		puissance *= 2;
+		i--;
+	}
+
+	return (resultat);
 }
