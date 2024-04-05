9002a26852c7f4366c8533aaa9c804f6f444c060
Ezio-33 <8691@holbertonstudents.com>
2024-04-05 13:12:20 +0000

    C - Bit manipulation tasks 1 correction

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,17 +1,15 @@
 #include "main.h"
-
 /**
- * print_binary - Imprime la représentation
- * binaire d'un nombre non signé de type
- * unsigned long int.
- * @n: Le nombre à imprimer.
+ * print_binary - Print binary
+ * representation of a number
+ * @n: The number to convert
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
+	if (n >> 0)
 	{
-		print_binary(n >> 1);
-
+		if (n >> 1)
+			print_binary(n >> 1);
 		_putchar((n & 1) + '0');
 	}
 	else
